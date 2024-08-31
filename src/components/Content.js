eee892411b6a446107d724e1f7f831394885766e
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2024-08-31 10:22:36 +0200

    usestate

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Content = () => {
-    const handlaName = () => {
-        const names = ['John', 'Paul', 'George', 'Ringo'];
-        const randomIndex = Math.floor(Math.random() * names.length);
-        return names[randomIndex];
-     }
 
-     const handleClick = () => {
-            alert('Hello, World!');
-     };
+  const [name, setName] = useState("Dariusz");
+  const handlaName = () => {
+    const names = ["John", "Paul", "George", "Ringo"];
+    const randomIndex = Math.floor(Math.random() * names.length);
+    setName(names[randomIndex]);
+  };
+
+  const handleClick = () => {
+    alert("Hello, World!");
+  };
+
+  const handleClick2 = (name) => {
+    alert("Hello, " + name);
+  };
+
+  const handleClick3 = (e) => {
+    alert("Hello, " + e.target.innerText);
+  };
   return (
     <main>
-        <p>
-    My name is {handlaName()}
-    </p>
-    <button onClick={handleClick}>Click me!</button>
+      <p>My name is {name}</p>
+      <button onClick={handlaName}>Change me!</button>
+      <button
+        onClick={() => {
+          handleClick2("Dariusz");
+        }}
+      >
+        Click me!
+      </button>
+      <button
+        onClick={(e) => {
+          handleClick3(e);
+        }}
+      >
+        Click me!
+      </button>
     </main>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
